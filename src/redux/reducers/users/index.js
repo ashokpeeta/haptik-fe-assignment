3da9275c18a976dab5b54942bb420fad715744ae
Ashok Peeta <ashok.peeta@cawstudios.com>
2021-08-02 13:20:40 +0530

    favorite, un-favorite, delete, add user actions addition

--- src/redux/reducers/users/index.js
+++ src/redux/reducers/users/index.js
@@ -27,6 +27,51 @@ function usersReducer(state = initialState, action) {
         users: []
       }
     }
+    case Actions.ADD_USER: {
+      const users = [ ...state.users ]
+      users.push(action.payload)
+      return {
+        ...state,
+        loading: false,
+        users
+      }
+    }
+    case Actions.DELETE_USER: {
+      const users = [ ...state.users ]
+      users.splice(action.payload, 1)
+      return {
+        ...state,
+        loading: false,
+        users
+      }
+    }
+    case Actions.TOGGLE_FAVORITE_USER: {
+      const { userIndex, actionIndex } = action.payload
+      const users = [ ...state.users ]
+      const toggledUser = users.splice(userIndex, 1)[0]
+      const favoriteUsers = users.filter((user) => {
+        return user.actions.filter((action) => {
+          return action.type === 'favorite'
+        }).length > 0
+      })
+      const nonFavoriteUsers = users.filter((user) => {
+        return user.actions.filter((action) => {
+          return action.type === 'non-favorite'
+        }).length > 0
+      })
+      if (toggledUser.actions[actionIndex].type === 'favorite') {
+        toggledUser.actions[actionIndex].type = 'non-favorite'
+        nonFavoriteUsers.push(toggledUser)
+      } else {
+        toggledUser.actions[actionIndex].type = 'favorite'
+        favoriteUsers.push(toggledUser)
+      }
+      return {
+        ...state,
+        loading: false,
+        users: favoriteUsers.concat(nonFavoriteUsers)
+      }
+    }
     default: {
       return state
     }
